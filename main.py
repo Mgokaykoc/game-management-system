2e426fabe9ed5bd573b4be6ad687851c5e833e12
Mgokaykoc <kocmehmetgokay@gmail.com>
2025-05-03 23:22:04 +0300

    Webserver ve database connection kuruldu

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from sanic import Sanic
+from sanic import Sanic, Blueprint
 from sanic.response import text
+
+import User.UserBlueprint
+from User import UserBlueprint
+
 
 from atlas import init_db
 
 app = Sanic("SanicExample")
+app.blueprint(User.UserBlueprint.user_blueprint)
 
 # Sanic sunucusu başlatılmadan ÖNCE init_db'yi çalıştırıyoruz
 @app.before_server_start
@@ -12,5 +17,5 @@ async def setup_database(app, loop):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True, single_process=True)
 
